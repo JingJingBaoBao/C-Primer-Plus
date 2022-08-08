a54d2c89b7494d0b04d30f168cab938d36764966
Sundaxian <m17824832870@163.com>
2022-08-08 23:46:21 +0800

    I will finish this shit

---
+++ chapter_7/7_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void simple();
+
+int main(void)
+{
+    using namespace std;
+    cout << "main() will the simple() function\n";
+    simple();
+    cout << "main() finish the simple() function\n";
+
+    return 0;
+}
+
+void simple()
+{
+    using namespace std;
+    cout << "I'm but a simple funtion.\n";
+}
---
+++ chapter_7/7_3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void n_chars(char, int);
+
+int main()
+{
+    int times;
+    char ch;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+    while (ch != 'q') {
+        cout << "Enter a int: ";
+        cin >> times;
+        n_chars(ch, times);
+        cout << "\nEnter another: ";
+        cin >> ch;
+    }
+    cout << "The value of times is " << times << ".\n";
+    return 0;
+}
+
+void n_chars(char c, int n)
+{
+    while (n--)
+    {
+        cout << c;
+    }
+    
+}
